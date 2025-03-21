e5cef73faca5102844552291ac2bbb49a3579e99
Reduktor Dev <reduktorsh@gmail.com>
2025-03-22 01:11:23 +0800

    Add files via upload

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = {
+  extends: "next/core-web-vitals",
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-empty-object-type": "off"
+  }
+};
+
+
+export default eslintConfig;
