53efe8eccea4dfd270477dc98b1eebd7a085f017
Reduktor Dev <reduktorsh@gmail.com>
2025-03-22 00:58:46 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: true, // jika kamu pakai fitur ini
+  },
   images: {
     remotePatterns: [
       {
